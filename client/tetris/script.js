744683320c9977e07f9a40a5f0bfa9a97f96fc86
Felipe Martinez <71576642+Felii-Martinez@users.noreply.github.com>
2022-09-30 19:12:45 -0400

    changes to java script

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const width = 10
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+    
+  console.log(squares)
 
 
 
